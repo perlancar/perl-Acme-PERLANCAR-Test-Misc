427dd72f95ad43838adec26f229a75ffaf361f38
perlancar (on pc-home) <perlancar@gmail.com>
2022-05-08 19:17:19 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Test/Misc.pm
@@ -0,0 +1,11 @@
+package Acme::PERLANCAR::Test::Misc;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Test various things
---
+++ lib/Acme/PERLANCAR/Test/Misc/Module2.pm
@@ -0,0 +1,11 @@
+package Acme::PERLANCAR::Test::Misc::Module2;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Just another module
